7d27f7973271e074b51c39ee581a1d511da5c6a6
Jason Livesay <ithkuil@gmail.com>
2015-05-09 01:20:01 +0000

    simpler

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-Code from the tutorial at [blog.mgechev.com](http://blog.mgechev.com/2014/09/03/webrtc-peer-to-peer-chat-with-react/)
-
-# Instructions
-
-```bash
-npm install
-npm install -g bower
-bower install
-node index.js
-open http://localhost:3001
-```
+React/webrtc demo for gracescale
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var PeerServer = require('peer').PeerServer;
 var express = require('express');
-var Topics = require('./public/src/Topics.js');
 var app = express();
 var port = process.env.PORT || 3001;
 
@@ -13,14 +11,3 @@ var io = require('socket.io').listen(expressServer);
 
 console.log('Listening on port', port);
 
-var peerServer = new PeerServer({ port: 9000, path: '/chat' });
-
-peerServer.on('connection', function (id) {
-  io.emit(Topics.USER_CONNECTED, id);
-  console.log('User connected with #', id);
-});
-
-peerServer.on('disconnect', function (id) {
-  io.emit(Topics.USER_DISCONNECTED, id);
-  console.log('With #', id, 'user disconnected.');
-});
