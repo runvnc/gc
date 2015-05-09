885450077c762f4996fec7f2e6792580d441dbcb
Jason Livesay <ithkuil@gmail.com>
2015-05-09 03:42:09 +0000

    es6 todo working

--- public/app.js
+++ public/app.js
@@ -1,18 +1,20 @@
 "use strict";
 
-var _prototypeProperties = function (child, staticProps, instanceProps) { if (staticProps) Object.defineProperties(child, staticProps); if (instanceProps) Object.defineProperties(child.prototype, instanceProps); };
-
 var _get = function get(object, property, receiver) { var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc && desc.writable) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _prototypeProperties = function (child, staticProps, instanceProps) { if (staticProps) Object.defineProperties(child, staticProps); if (instanceProps) Object.defineProperties(child.prototype, instanceProps); };
 
 var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
 
 var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
 var TodoList = (function (_React$Component) {
-  function TodoList(props) {
+  function TodoList() {
     _classCallCheck(this, TodoList);
 
-    _get(Object.getPrototypeOf(TodoList.prototype), "constructor", this).call(this, props);
+    if (_React$Component != null) {
+      _React$Component.apply(this, arguments);
+    }
   }
 
   _inherits(TodoList, _React$Component);
@@ -20,7 +22,7 @@ var TodoList = (function (_React$Component) {
   _prototypeProperties(TodoList, null, {
     render: {
       value: function render() {
-        var createItem = function (itemText, index) {
+        function createItem(itemText, index) {
           return React.createElement(
             "li",
             { key: index + itemText },
@@ -42,10 +44,11 @@ var TodoList = (function (_React$Component) {
 })(React.Component);
 
 var TodoApp = (function (_React$Component2) {
-  function TodoApp() {
+  function TodoApp(props) {
     _classCallCheck(this, TodoApp);
 
-    return { items: [], text: "" };
+    _get(Object.getPrototypeOf(TodoApp.prototype), "constructor", this).call(this, props);
+    this.state = { items: props.items, text: props.text };
   }
 
   _inherits(TodoApp, _React$Component2);
@@ -81,8 +84,9 @@ var TodoApp = (function (_React$Component2) {
           React.createElement(TodoList, { items: this.state.items }),
           React.createElement(
             "form",
-            { onSubmit: this.handleSubmit },
-            React.createElement("input", { onChange: this.onChange, value: this.state.text }),
+            { onSubmit: this.handleSubmit.bind(this) },
+            React.createElement("input", { onChange: this.onChange.bind(this),
+              value: this.state.text }),
             React.createElement(
               "button",
               null,
@@ -98,6 +102,10 @@ var TodoApp = (function (_React$Component2) {
 
   return TodoApp;
 })(React.Component);
+
+TodoApp.propTypes = { items: React.PropTypes.array,
+  text: React.PropTypes.string };
+TodoApp.defaultProps = { items: [], text: "" };
 
 React.render(React.createElement(TodoApp, null), document.getElementById("main"));
 
--- public/app.jsx
+++ public/app.jsx
@@ -1,9 +1,6 @@
 class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
-    var createItem = function(itemText, index) {
+    function createItem(itemText, index) {
       return <li key={index + itemText}>{itemText}</li>;
     };
     return <ul>{this.props.items.map(createItem)}</ul>;
@@ -11,30 +8,35 @@ class TodoList extends React.Component {
 }
 
 class TodoApp extends React.Component {
-  constructor() {
-    return {items: [], text: ''};
+  constructor(props) {
+    super(props)
+    this.state = {items: props.items, text: props.text};
   }
-    onChange(e) {
-      this.setState({text: e.target.value});
-    }
-    handleSubmit(e) {
-      e.preventDefault();
-      var nextItems = this.state.items.concat([this.state.text]);
-      var nextText = '';
-      this.setState({items: nextItems, text: nextText});
-    }
-    render() {
-      return (
+  onChange(e) {
+    this.setState({text: e.target.value});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  }
+  render() {
+    return (
         <div>
         <h3>TODO</h3>
         <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-        <input onChange={this.onChange} value={this.state.text} />
+        <form onSubmit={this.handleSubmit.bind(this)}>
+        <input onChange={this.onChange.bind(this)} 
+               value={this.state.text} />
         <button>{'Add #' + (this.state.items.length + 1)}</button>
         </form>
         </div>
         );
-    }
+  }
 }
+TodoApp.propTypes = {items: React.PropTypes.array,
+                     text: React.PropTypes.string};
+TodoApp.defaultProps = {items: [], text: ''};
 
 React.render(<TodoApp />, document.getElementById('main'));
